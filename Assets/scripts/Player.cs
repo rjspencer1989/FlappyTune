75bd0a5b465d688894a3aaa1661e7ca3abf84701
Robert Spencer <rob@robspencer.me.uk>
2016-05-17 22:11:29 +0100

    initial commit

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private Rigidbody2D rb;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		float input = Input.GetAxis ("Jump");
+		rb.AddForce (new Vector2(0.0f, input) * speed);
+	}
+		
+}
