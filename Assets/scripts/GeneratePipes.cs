7cbf8e04931de63376301d708d194b434ee7c150
Robert Spencer <rob@robspencer.me.uk>
2016-06-11 16:38:13 +0100

    pipes, pipes, everywhere there's pipes

---
+++ Assets/scripts/GeneratePipes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratePipes : MonoBehaviour {
+
+	public GameObject pipes;
+
+	// Use this for initialization
+	void Start()
+	{
+		InvokeRepeating("CreateObstacle", 1f, 1.5f);
+	}
+
+	void CreateObstacle()
+	{
+		Instantiate(pipes);
+	}
+}
---
+++ Assets/scripts/MovePipes.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePipes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (new Vector3 (-0.05f, 0, 0));
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		print (other.tag);
+		if(other.gameObject.CompareTag("Respawn")){
+			print ("respawn");
+			this.gameObject.SetActive(false);
+		}
+	}
+}
