641ce2e4f0ee9f1c81a92dae446b13d1bce294c1
Robert Spencer <rob@robspencer.me.uk>
2016-06-28 11:06:34 +0100

    fix #1

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 	private Rigidbody2D rb;
 	public float speed;
+	public Text scoreText;
+	int score = 0;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		setScoreText();
 	}
 	
 	// Update is called once per frame
@@ -24,5 +28,16 @@ public class Player : MonoBehaviour {
 			this.gameObject.SetActive(false);
 			SceneManager.LoadScene ("GameOver");
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("score")){
+			score += 1;
+			setScoreText();
+		}
+	}
+
+	void setScoreText(){
+		scoreText.text = "Score: " + score.ToString();
 	}
 }
