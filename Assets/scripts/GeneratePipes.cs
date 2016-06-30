6dba0f2be6458dceb3577827ff5b498db59df5d1
Robert Spencer <rob@robspencer.me.uk>
2016-06-30 22:08:15 +0100

    set pipe position randomly

--- Assets/scripts/GeneratePipes.cs
+++ Assets/scripts/GeneratePipes.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 
-public class GeneratePipes : MonoBehaviour {
+public class GeneratePipes : MonoBehaviour
+{
 
-	public GameObject pipes;
+    public GameObject pipes;
+    public float moveFactor = 1.0f;
+    float pipeMin;
 
-	// Use this for initialization
-	void Start()
-	{
-		InvokeRepeating("CreateObstacle", 1f, 1.5f);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        pipeMin = moveFactor * -1;
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+    }
 
-	void CreateObstacle()
-	{
-		Instantiate(pipes);
-	}
+    void CreateObstacle(){
+        print(moveFactor);
+        float rv = Random.Range(pipeMin, moveFactor);
+        print(rv);
+        Vector3 pos = new Vector3(8, rv, 0);
+
+        Instantiate(pipes, pos, Quaternion.identity);
+    }
 }
