8ed5afa56363e94980dff15cfbc7946563fc603d
Robert Spencer <rob@robspencer.me.uk>
2016-07-15 22:42:20 +0100

    positional stuff

--- Assets/scripts/GeneratePipes.cs
+++ Assets/scripts/GeneratePipes.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using System.Linq;
 
 public class GeneratePipes : MonoBehaviour
 {
@@ -9,30 +10,37 @@ public class GeneratePipes : MonoBehaviour
     float heightPerNote = 0.5f;
     float yPos = 1;
 
-    List<string> steps;
+    float lowPosition = 0.0f;
+    float highPosition = 0.0f;
+
+    int offset = 0;
 
     // Use this for initialization
     void Start()
     {
-        steps = new List<string>(new string[]{"C", "D", "E", "F", "G", "A", "B"});
         mainCamera = Camera.main;
+        highPosition = (mainCamera.orthographicSize - 0.5f);
+        lowPosition = -highPosition;
         string songName = Scenes.getParameter("songName");
         TextAsset asset = Resources.Load(songName) as TextAsset;
         string json = asset.text;
         Song song = JsonUtility.FromJson<Song>(json);
-        
-        //Find lowest note
-        //Find highest note
-        //Calculate difference
-        //divide visible height by number of notes
+        List<Note> sorted = song.notes.OrderBy(o=>o.pitch.getPitchValue()).ToList();
+        Note lowest = sorted.First();
+        Note highest = sorted.Last();
+        print(lowest.pitch.ToString());
+        print(highest.pitch.ToString());
+        offset = highest.pitch.getPitchOffset(lowest.pitch);
+        print(offset);
+        heightPerNote = (highPosition * 2) / offset;
+        print(heightPerNote);
         new WaitForSeconds(2.0f);
         StartCoroutine(CreateObstacle(song));
     }
 
     public IEnumerator CreateObstacle(Song song){
         foreach (Note item in song.notes){
-
-            yPos = heightPerNote * (steps.IndexOf(item.pitch.step) + 1);
+            yPos = lowPosition + (heightPerNote * item.pitch.getStepNum());
             Vector3 pos = new Vector3(8, yPos, 0);
 
             print(pos);
@@ -41,5 +49,10 @@ public class GeneratePipes : MonoBehaviour
 
             yield return new WaitForSeconds(1.5f);
         }
+
+        // keep score box fixed height
+        // keep score box bounds between min and max
+        // calculate mid point of score box
+        // pipes will be positioned appropriately
     }
 }
--- Assets/scripts/Song.cs
+++ Assets/scripts/Song.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 [Serializable]
 public class Song{
@@ -16,6 +18,23 @@ public class Note{
 
 [Serializable]
 public class Pitch{
+    List<string> steps = new List<string>(new string[]{"C", "D", "E", "F", "G", "A", "B"});
     public string step;
     public int octave;
+
+    public int getPitchValue(){
+        return (this.getStepNum() + 1) * this.octave;
+    }
+
+    public int getStepNum(){
+        return steps.IndexOf(this.step);
+    }
+
+    public int getPitchOffset(Pitch other){
+        return ((this.getStepNum() + 1 - (steps.IndexOf(other.step) + 1)) + (8 * (this.octave - other.octave)));
+    }
+
+    public override string ToString(){
+        return String.Format("{0}:{1}", this.step, this.octave);
+    }
 }
