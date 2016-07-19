e10e456e49556eb0e0584895dbb80a319398a5b9
Robert Spencer <rob@robspencer.me.uk>
2016-07-19 13:10:01 +0100

    parse xml as far as get list of measures

--- Assets/scripts/MusicParser/MusicParser.cs
+++ Assets/scripts/MusicParser/MusicParser.cs
@@ -1,15 +1,40 @@
 using System.Xml;
 using UnityEngine;
 public static class MusicParser {
-    static XmlDocument score;
-    public static void LoadFile(string name){
+    public static XmlDocument LoadFile(string name){
         TextAsset asset = Resources.Load(name) as TextAsset;
-        score = new XmlDocument();
-        score.LoadXml(asset.text);
+        XmlDocument scoreDocument = new XmlDocument();
+        scoreDocument.LoadXml(asset.text);
+        return scoreDocument;
     }
 
     public static void ParseScore(string name){
-        LoadFile(name);
+        XmlDocument doc = LoadFile(name);
+        Score score = new Score();
+        XmlNodeList partNodes = doc.SelectNodes("score-partwise/part-list/part");
+        if(partNodes != null){
+            foreach (XmlNode partNode in partNodes){
+                Part part = new Part();
+                score.Parts.Add(part);
+                if(partNode.Attributes != null){
+                    part.Id = partNode.Attributes["id"].InnerText;
+                }
 
+                XmlNode partName = partNode.SelectSingleNode("part-name");
+                if(partName != null){
+                    part.Name = partName.InnerText;
+                }
+
+                string measurePath = string.Format("//part[@id='{0}']/measure", part.Id);
+                XmlNodeList measureNodes = partNode.SelectNodes(measurePath);
+
+                if(measureNodes != null){
+                    foreach (XmlNode measureNode in measureNodes){
+                        Measure measure = new Measure();
+                        
+                    }
+                }
+            }
+        }
     }
 }
