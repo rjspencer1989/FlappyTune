df165fb74e2ec8b569395fd63f6ca050637b1065
Robert Spencer <rob@robspencer.me.uk>
2016-07-21 10:53:52 +0100

    fix null reference exception

--- Assets/scripts/MusicParser/Song.cs
+++ Assets/scripts/MusicParser/Song.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 public class Song {
     internal Song() {
-        List<MeasureElement> MeasureElements = new List<MeasureElement>();
+        MeasureElements = new List<MeasureElement>();
     }
 
     public List<MeasureElement> MeasureElements { get; internal set; }
