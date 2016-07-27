a70c730af1294d71e77e5252b4d7d18adca83cc7
Robert Spencer <rob@robspencer.me.uk>
2016-07-27 22:42:08 +0100

    don't need anything else

--- Assets/scripts/MovePipes.cs
+++ Assets/scripts/MovePipes.cs
@@ -1,13 +1,6 @@
 ﻿using UnityEngine;
 
 public class MovePipes : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void FixedUpdate () {
 		this.transform.Translate (new Vector3 (-0.05f, 0, 0));
 	}
